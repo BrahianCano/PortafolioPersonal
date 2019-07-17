4b72fe9b28336da1e0b2e66c46bc9019328923ef
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-17 12:45:44 -0500

    Completado

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+
+//Funcion flecha
+const About = () =>(<h1>Hola mundo geek</h1>);
+
+export default About;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/About';
+
+//Que quiero renderisar.
+//Donde lo voy a renderisar.
+ReactDOM.render(<About/> , document.getElementById('app'));
